17bf5df65a41426760e572837f460ca7b8496260
Hernah Grace Alosada <hgalosada@gmail.com>
2022-12-17 13:08:02 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,8 +1,10 @@
 package FoodOrdering;
 
-import SimpleCalc.SimpleCalcGUI;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -18,7 +20,44 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    private List<JRadioButton> discount;
+    private List<JCheckBox> food;
     public FoodOrderGUI() {
+        int[] prices = {100,80,65,55,50,40};
+        double[] rate = {0,0.05,0.10,0.15};
+        discount = new ArrayList<>();
+        discount.add(rbNone);
+        discount.add(rb5);
+        discount.add(rb10);
+        discount.add(rb15);
+        food = new ArrayList<>();
+        food.add(cPizza);
+        food.add(cBurger);
+        food.add(cFries);
+        food.add(cSoftDrinks);
+        food.add(cTea);
+        food.add(cSundae);
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int i = 0;
+                double total = 0;
+                for(JCheckBox f: food) {
+                    if(f.isSelected()) {
+                        total+=prices[i];
+                    }
+                    i++;
+                }
+                i = 0;
+                for(JRadioButton d: discount) {
+                    if(d.isSelected()) {
+                        total = total - (total * rate[i]);
+                    }
+                    i++;
+                }
+                JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", total));
+            }
+        });
     }
 
     public static void main(String[] args) {
